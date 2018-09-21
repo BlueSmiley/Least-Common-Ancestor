6b5f971a393f95f4d8f766d23c0e47259048b58f
BlueSmiley <BlueSmiley@users.noreply.github.com>
2018-09-21 13:29:05 +0100

    LCATest added

---
+++ LCATest.py
@@ -0,0 +1,5 @@
+#PyUnit test code
+#No experience writing such code and havent made code yet
+#So making comments on how to structure the tests
+#Remove after code complete
+#Also good test for using git with linux
