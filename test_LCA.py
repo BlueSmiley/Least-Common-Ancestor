d62010262172b212f663903a210f65f9f9f6469a
BlueSmiley <BlueSmiley@users.noreply.github.com>
2018-09-23 16:43:49 +0100

    better unittest comments

--- test_LCA.py
+++ test_LCA.py
@@ -25,23 +25,21 @@ class LCATest(unittest.TestCase):
         graph = LCAGraph()
         root = 0
         graph.add_node(root)
-        self.assertItemsEqual(graph.edges(root),[])
+        self.assertItemsEqual(graph.edges(root),[],"checking when root empty")
         graph.add_edge(root,1)
-        self.assertItemsEqual(graph.edges(root),[1])
-        self.assertItemsEqual(graph.edges(1),[])
+        self.assertItemsEqual(graph.edges(root),[1],"root one child")
+        self.assertItemsEqual(graph.edges(1),[],"leaf node")
         graph.add_edge(root,1)
-        self.assertItemsEqual(graph.edges(root),[1])
-        #No assertItemsNotEqual so have to do this
-        self.assertNotEqual(Counter(iter(graph.edges(root))),  
-                     Counter(iter([1,1])))
+        self.assertItemsEqual(graph.edges(root),[1],"no duplicate edges")
         graph.add_edge(root,2)
-        self.assertItemsEqual(graph.edges(root),[1,2])
+        self.assertItemsEqual(graph.edges(root),[1,2],"root two children")
         graph.add_edge(1,3)
         graph.add_edge(1,5)
         graph.add_edge(2,4)
-        self.assertItemsEqual(graph.edges(1),[5,3])
-        self.assertItemsEqual(graph.edges(1),[3,5])
-        self.assertItemsEqual(graph.edges(2),[4])
+        self.assertItemsEqual(graph.edges(1),[5,3],"non-root two children")
+        self.assertItemsEqual(graph.edges(2),[4]),"non-root one child"
+        graph.add_edge(1,6)
+        self.assertItemsEqual(graph.edges(1),[3,5],"root third child ignored")
 
 
 
@@ -57,17 +55,30 @@ class LCATest(unittest.TestCase):
         graph.add_edge(2,5)
         graph.add_edge(3,6)
         graph.add_edge(3,7)
-        self.assertEqual(graph.lowest_common_ancestor(root,4,5),2)
-        self.assertEqual(graph.lowest_common_ancestor(root,4,6),1)
-        self.assertEqual(graph.lowest_common_ancestor(root,3,4),1)
-        self.assertEqual(graph.lowest_common_ancestor(root,2,4),2)
-        self.assertEqual(graph.lowest_common_ancestor(root,1,4),1)
+        self.assertEqual(graph.lowest_common_ancestor(root,4,5),2,
+        "two leaf nodes same parent")
+        self.assertEqual(graph.lowest_common_ancestor(root,4,6),1,
+        "two leaf nodes different parent")
+        self.assertEqual(graph.lowest_common_ancestor(root,3,4),1,
+        "one leaf node and one non-leaf node")
+        self.assertEqual(graph.lowest_common_ancestor(root,2,4),2,
+        "parent and child leaf node")
+        self.assertEqual(graph.lowest_common_ancestor(root,1,4),1,
+        "leaf node and root with params swapped")
         graph.add_node(9)
-        self.assertIsNone(graph.lowest_common_ancestor(root,2,8))
-        self.assertIsNone(graph.lowest_common_ancestor(root,1,9))
-        self.assertEqual(graph.lowest_common_ancestor(root,1,1),1)
-        self.assertEqual(graph.lowest_common_ancestor(root,2,1),1)
-        self.assertEqual(graph.lowest_common_ancestor(root,2,2),2)
+        #checking doesnt give false positives
+        self.assertIsNone(graph.lowest_common_ancestor(root,2,8),
+        "lca non-existent node")
+        self.assertIsNone(graph.lowest_common_ancestor(root,1,9),
+        "lca disconnected node")
+        #checking gives correct answer if two params same
+        self.assertEqual(graph.lowest_common_ancestor(root,1,1),1,
+        "root and both nodes root")
+        self.assertEqual(graph.lowest_common_ancestor(root,2,1),1,
+        "root and child node with lca = root")
+        self.assertEqual(graph.lowest_common_ancestor(root,2,2),2,
+        "child with both nodes child")
+
 
 if __name__ == "__main__":
     unittest.main()
